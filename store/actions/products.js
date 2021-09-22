89cd856447b5e6186a475edb7a14f8bfc0c0d191
HeadJk <jake4head@outlook.com>
2021-09-22 16:43:09 -0230

    Created firebase database and added create and retrieve product functionality with the database

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,20 +1,67 @@
+import Product from '../../models/product';
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCT = 'SET_PRODUCT';
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    // Any async code you want
+    const response = await fetch('https://rn-complete-guide-25b5d-default-rtdb.firebaseio.com/products.json')
+
+    const resData = await response.json();
+    const loadedProducts = [];
+    for(const key in resData) {
+      loadedProducts.push(new Product(
+        key, 
+        'u1',
+        resData[key].title,
+        resData[key].imageUrl,
+        resData[key].description,
+        resData[key].price
+        ))
+    }
+
+    console.log(resData);
+
+    dispatch({ type: SET_PRODUCT, products: loadedProducts })
+  }
+}
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    // Any async code you want
+    const response = await fetch('https://rn-complete-guide-25b5d-default-rtdb.firebaseio.com/products.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    })
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch ({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    })
   };
 };
 
